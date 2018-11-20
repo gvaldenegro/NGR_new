4903085b3ad278869b941b9c193530e88fcc5a14
Sebastián Ortega <sortegapizarro@gmail.com>
2018-11-20 10:28:58 -0300

    Vinculo menu camaras
    Se añaden el link de cada categoria de camara a su simil de woocommerce

--- assets/js/custom-map.js
+++ assets/js/custom-map.js
@@ -13,31 +13,31 @@
         // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
         var mapOptions = {
             // How zoomed in you want the map to start at (always required)
-            zoom: 12,
+            zoom: 17,
 
             // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(40.6700, -73.9400), // New York
+            center: new google.maps.LatLng(-33.5332738,-70.6633298), // New York
 
             scrollwheel: false,
 
             // How you would like to style the map.
             // This is where you would paste any style found on Snazzy Maps.
-            styles: [
-            {"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},
-            {"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},
-            {"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},
-            {"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
-            {"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},
-            {"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},
-            {"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},
-            {"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},
-            {"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},
-            {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},
-            {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},
-            {"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},
-            {"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},
-            {"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},
-            {"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
+            // styles: [
+            // {"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},
+            // {"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},
+            // {"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},
+            // {"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
+            // {"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},
+            // {"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},
+            // {"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},
+            // {"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},
+            // {"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},
+            // {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},
+            // {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},
+            // {"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},
+            // {"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},
+            // {"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},
+            // {"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
         };
 
         // Get the HTML DOM element that will contain your map
@@ -49,7 +49,7 @@
 
         // Let's also add a marker while we're at it
         var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(40.6700, -73.9400),
+            position: new google.maps.LatLng(-33.5332738,-70.6633298),
             map: map,
             title: 'Snazzy!'
         });
